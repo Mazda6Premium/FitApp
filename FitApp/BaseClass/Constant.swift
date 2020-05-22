14b8e487f1bc9847cc825d09ad930c829fdc013f
Trung iOS <trungnt@vps.com.vn>
2020-05-22 20:18:50 +0700

    setup project

---
+++ FitApp/BaseClass/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  FitApp
+//
+//  Created by Trung iOS on 5/22/20.
+//  Copyright © 2020 Fighting. All rights reserved.
+//
+
+import Foundation
---
+++ FitApp/BaseClass/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  FitApp
+//
+//  Created by Trung iOS on 5/22/20.
+//  Copyright © 2020 Fighting. All rights reserved.
+//
+
+import Foundation
